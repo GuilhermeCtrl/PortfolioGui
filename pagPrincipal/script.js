034035256e59942cde0d8de25bbeb23cdff17f1d
Guilherme <jcg5aquino@gmail.com>
2024-09-20 12:17:31 -0300

    refatoracao do JS

--- pagPrincipal/script.js
+++ pagPrincipal/script.js
@@ -1,16 +1,18 @@
-// JavaScript Adicional para Abrir e Fechar o Modal
-function abrirSobreMim() {
+const btnAbrirMim = document.getElementById("btnOpenEu")
+const btnFecharMim = document.getElementById("btnCloseMim")
+
+btnAbrirMim.addEventListener("click", function(){
     var modal = document.getElementById("sobreMimModal");
     modal.style.display = "flex";
     setTimeout(function() {
         modal.classList.add("show");
     }, 10);
-}
+})
 
-function fecharSobreMim() {
+btnFecharMim.addEventListener("click", function(){
     var modal = document.getElementById("sobreMimModal");
     modal.classList.remove("show");
     setTimeout(function() {
         modal.style.display = "none";
-    }, 400); // Tempo de espera para fechar após a animação
-}
+    }, 400);
+})
